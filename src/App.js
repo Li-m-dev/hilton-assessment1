3158b0a2d7cf09f0162955cb2266a6b3e2734a9c
Li Swafford <liswafford@yahoo.com>
2019-06-09 00:24:29 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./test1_assets/HI_mk_logo_hiltonbrandlogo_3.jpg";
+import hotelExterior from "./test1_assets/hotelexterior.jpg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>&lt; Back </button>
+        <h3>Hotel Details</h3>
+        <img src={logo} alt="logo" />
       </header>
-    </div>
+      <div className="main-content">
+        <img src={hotelExterior} alt="hotel" />
+        <div className="contact-info">
+          <h3>Hilton Chicago</h3>
+          <p>
+            720 South Michigan Avenue
+            <br />
+            Chicago, Illinois, 60605
+          </p>
+          <a href="tel:+1-312-922-4400">1-312-922-4400</a>
+        </div>
+        <div className="more-info">
+          <ul>
+            <li>
+              <p>Map</p> <p>&gt;</p>
+            </li>
+            <li>
+              <p>Photo gallery</p>
+              <p>&gt;</p>
+            </li>
+            <li>
+              <p>Amenities</p>
+              <p>&gt;</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <footer>Welcome to Hilton!</footer>
+    </>
   );
 }
 
